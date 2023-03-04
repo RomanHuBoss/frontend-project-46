62604339ee74504ecba4cd1925504b3998b8c40d
Roman Rabinovich <roman@web-line.ru>
2023-03-04 16:59:52 +0300

    Refactoring of command line parser

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,21 +1,54 @@
 import { Command } from 'commander';
+import * as fs from 'node:fs';
+import * as path from 'path';
 
-const program = new Command();
+const getFileData = (filepath) => {
+  if (filepath[0] !== '/') {
+    filepath = path.resolve(filepath);
+  }
 
-program
+  try {
+    return fs.readFileSync(filepath, "utf8");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getFileExtension = (filepath) => {
+  return filepath.split('.').pop();
+};
+
+const gendiff = (filepath1, filepath2) => {
+  const rawFileData1 = getFileData(filepath1);
+  const rawFileData2 = getFileData(filepath2);
+
+  if (getFileExtension(filepath1) === 'json') {
+    JSON.parse(rawFileData1);
+  }
+
+  console.log(rawFileData1);
+  console.log(rawFileData2);
+};
+
+
+//разбор командной строки
+((program) => {
+  
+  program
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action(() => {
+    gendiff(...program.processedArgs);
+  });
 
-program.parse();
+  program.parse();  
+  
+  return program.processedArgs;
 
-const options = program.opts();
-
-const gendiff = (filepath1, filepath2) => {
-
-};
+})(new Command());
 
 export default gendiff;
 
